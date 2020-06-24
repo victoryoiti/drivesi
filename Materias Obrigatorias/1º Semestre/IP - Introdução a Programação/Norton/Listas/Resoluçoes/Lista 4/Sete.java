5f64da638479a000784e6abbb7fb7d00447981fa
victoryoiti <victoryoiti@hotmail.com>
2020-06-24 13:39:38 -0300

    Update Sete.java
    uma pequena correção

--- Materias Obrigatorias/1º Semestre/IP - Introdução a Programação/Norton/Listas/Resoluçoes/Lista 4/Sete.java
+++ Materias Obrigatorias/1º Semestre/IP - Introdução a Programação/Norton/Listas/Resoluçoes/Lista 4/Sete.java
@@ -1,16 +1,15 @@
-class Sete {
-  public static boolean Inverso (char [] num) {
-  boolean teste = false;
-    for (int i = 0; i<num.length /2; i++) {
-      if (num [i] == num[num.length - 1 - i]) {
-       teste = true;
+class l4ex7 {
+  public static boolean Inverso (int [] num) {
+  boolean teste = true;
+    for (int i = 0; i<num.length/2; i++) {
+      if (num [i] != num[num.length - 1 - i]) {
+      teste = false;
       }
-       else teste=false;
     }
     return teste;
   }
   public static void main (String [] args) {
-    char [] num1 = {'2', '4', '2','1', '2','4', '2'};
+    int [] num1 = {2,4,4,4,2};
     System.out.println (Inverso (num1));
   }
 }
